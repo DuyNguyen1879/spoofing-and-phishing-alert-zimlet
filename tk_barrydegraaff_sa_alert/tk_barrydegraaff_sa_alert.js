7b3ae907315aa718f0e7c55e94b7fae40c94e241
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2019-11-21 19:50:08 +0100

    Replace popup box with info pane

--- tk_barrydegraaff_sa_alert/tk_barrydegraaff_sa_alert.js
+++ tk_barrydegraaff_sa_alert/tk_barrydegraaff_sa_alert.js
@@ -141,24 +141,37 @@ SA_AlertZimlet.prototype.onMsgView = function (msg, oldMsg, view) {
             return;
          }
 
-         SA_AlertZimlet.prototype._dialog = new ZmDialog( { title:this.getMessage("saAlert_popup_title"), parent:this.getShell(), standardButtons:[DwtDialog.OK_BUTTON], disposeOnPopDown:true } );
          var alertmailTxt = "";
          if((alertmail) && (alertedIds.indexOf(","+msg.id))<0)
          {
             alertmailTxt = this.getMessage("saAlert_message_forwarded_to") + " " + alertmail;
          }
-         SA_AlertZimlet.prototype._dialog.setContent(
-         '<b>' +
-         this.getMessage("saAlert_phishing_chance") + '<br>' +
-         '<ul>' +
+         infoPane = new DwtComposite({
+                 parent: view
+         });
+         infoPaneLabel = new DwtLabel({
+            parent: infoPane,
+            style: DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_LEFT
+         });
+         infoPaneLabel.addClassName("InfoBox");
+         infoPaneLabel.setSize(Dwt.DEFAULT,"30em");
+         infoPaneLabel.setText('<div style="height:30em;background-color:yellow;border-style:solid;border-color:red;"><br/>' +
+         '<p style="text-align:center;font-size:18px;">' + '<b>' + this.getMessage("saAlert_popup_title") + '</p>' +
+         '<br/>' +
+         '<p style="text-align:center;font-size:18px;">' + '<b>' + this.getMessage("saAlert_phishing_chance") + '</p>' +
+         '<br/>' +
+         '<ul style="font-size:14px;">' +
          '<li>' + this.getMessage("saAlert_not_links") + '</li>' +
          '<li>' + this.getMessage("saAlert_not_download") + '</li>' +
          '<li>' + this.getMessage("saAlert_not_open") + '</li>' +
          '<li>' + this.getMessage("saAlert_not_reply") + '</li>' +
          '<li>' + this.getMessage("saAlert_not_forward") + '</li>' +
          '</ul>' +
-         '</b>' + '</b><br><br> '+this.getMessage("saAlert_mark_as_spam")+'.<br>' + alertmailTxt);
-         SA_AlertZimlet.prototype._dialog.popup();
+         '</b>' + '<br>'+ '<p style="text-align:center;font-size:14px;">' + this.getMessage("saAlert_mark_as_spam")+'.<br>' + alertmailTxt + '</p><br/></div>');
+         infoPane.addChild(infoPaneLabel);
+
+         el = view.getHtmlElement();
+         el.insertBefore(infoPane.getHtmlElement(), el.firstChild);
          if((alertmail) && (alertedIds.indexOf(","+msg.id))<0)
          {
             SA_AlertZimlet.prototype.notifyAttach(msg.id);
